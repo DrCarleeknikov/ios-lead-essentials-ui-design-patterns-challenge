9ed671745c60bd7ce7c15a46f33f9767b7ffb220
Joshua Bryson <joshua.bryson@medbridgeed.com>
2021-09-21 20:50:11 -0600

    test screens when the error is shown MVVM

--- MVVM Module/Source/Feed UI/Controllers/FeedViewController.swift
+++ MVVM Module/Source/Feed UI/Controllers/FeedViewController.swift
@@ -5,7 +5,7 @@
 import UIKit
 
 public final class FeedViewController: UITableViewController, UITableViewDataSourcePrefetching {
-	@IBOutlet private weak var errorView: ErrorView!
+	@IBOutlet private(set) weak var errorView: ErrorView!
 
 	var viewModel: FeedViewModel? {
 		didSet { bind() }
