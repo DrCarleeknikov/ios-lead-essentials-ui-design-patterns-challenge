857fc6976aa8c8a2d48ba63fa029f67154abbffb
JoshB <bryson.joshua@gmail.com>
2021-10-10 12:30:41 -0600

    make error view getter public in the MVP section

--- MVP Module/Tests/Feed UI/Helpers/FeedViewController+TestHelpers.swift
+++ MVP Module/Tests/Feed UI/Helpers/FeedViewController+TestHelpers.swift
@@ -62,10 +62,6 @@ extension FeedViewController {
 		return 0
 	}
 
-	private var errorView: ErrorView? {
-		tableView.tableHeaderView as? ErrorView
-	}
-
 	var errorMessage: String? {
 		return errorView?.message
 	}
